c583f014b1602693ec26fc58f83b3202c727ce15
Oliver Hargreaves <oliver.hargreaves@frozenmountain.com>
2023-09-21 15:55:48 -0400

    Add Video class

---
+++ src/components/Video.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="video-wrapper" :style="{height: calcHeight, width: calcWidth}">
+        <v-img class="video" cover src="@/assets/headshot.jpeg" :style="{height: calcHeight, width: calcWidth}"/>
+        <div class="video-overlay" style="background-color: #03011d; opacity: 60%;"></div>
+        <div class="video-overlay" style="justify-content: center;">
+            <div class="video-controls">
+                <i class="icon-video margin icon"/>
+                <i class="icon-audio-mic margin icon"/>
+            </div>
+        </div>
+        <div class="video-overlay" style="justify-content: end;">
+            <v-switch class="margin vcenter" label="Pin" color="blue" style="color: white;" v-if="userName == 'Me'"></v-switch>
+            <v-btn class="margin vcenter" v-if="userName">{{userName}}</v-btn>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import { type PropType, ref, onMounted, computed, getCurrentInstance, watch, onUnmounted } from "vue";
+    const props = defineProps({
+		userName: {
+			type: String,
+		},
+        userCount: {
+            type: Number
+        },
+        askHeight: {
+            type: String
+        },
+        askWidth: {
+            type: String
+        }
+	});
+    var calcHeight = props.askHeight || "375px"
+    var calcWidth = props.askWidth || "640px"
+    
+</script>
+
+<style lang="scss" scoped>
+    .video-controls {
+		position: absolute;
+		bottom: 0;
+		border-radius: 0 0 15px 15px;
+		height: 100%;
+	}
+    .video {
+        border-radius: 15px;
+        margin: 0 25px 0 0;
+    }
+    .video-wrapper {
+        border-radius: 5px;
+        position: relative;
+    }
+    .video-controls {
+        display: flex;
+        justify-content: space-around;
+    }
+    .video-overlay {
+        display: flex;
+        flex-direction: row;
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        height: 15%;
+        border-radius: 0 0 15px 15px;
+    }
+    .icon {
+        color: white;
+        font-size: 40px;
+    }
+    @media (max-width: 600px) or (max-height: 1000px) {
+        .icon {
+            color: white;
+            font-size: 28px;
+        }
+    }
+</style>
